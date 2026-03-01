b7aa00f0deb73486127600592267a471d4031463
Jamie <jamiependergast@gmail.com>
2026-03-01 00:07:23 +0000

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,25 @@
 #include <iostream>
 #include <span>
 
+// gpu
 #include <cyrex_voxels/gfx/vertex_array.h>
 #include <cyrex_voxels/gfx/vertex_buffer.h>
 #include <cyrex_voxels/gfx/index_buffer.h>
 #include <cyrex_voxels/gfx/drawing.h>
 #include <cyrex_voxels/gfx/shader.h>
 
-#include <cyrex_voxels/vox/samplers.h>
-#include <cyrex_voxels/vox/blocky.h>
+// rendering
 #include <cyrex_voxels/rend/camera.h>
 
+
+// voxel library
 #include <cyrex_voxels/vox/transform.h>
 #include <cyrex_voxels/vox/convert.h>
+#include <cyrex_voxels/vox/pipeline.h>
+#include <cyrex_voxels/vox/samplers.h>
+#include <cyrex_voxels/vox/blocky.h>
 
 #include <glm/gtx/hash.hpp>
-#include <glm/gtc/noise.hpp>
-
-#include "cyrex_voxels/vox/convert.h"
 
 constexpr std::string_view test_vertex_source = R"(
 #version 460 core
@@ -73,15 +75,25 @@ auto hash01(const vox::Coord coord) {
 }
 
 template<>
-struct vox::voxel_traits<MyVoxel> {
-    [[nodiscard]] constexpr static bool is_solid(const MyVoxel voxel) {
+struct vox::voxel_mesh_traits<MyVoxel> {
+    [[nodiscard]] constexpr static bool is_visible(const MyVoxel voxel) {
         return voxel != MyVoxel::None;
+    }
+
+
+    static glm::vec3 rainbow(const Coord coord) {
+        const float pi = glm::pi<float>();
+        const float shift = glm::radians(120.0f);
+        const float radians = glm::radians(float(coord.y)) * pi;
+
+        return glm::vec3(0.5f) + glm::vec3(0.5f) *
+            glm::sin(glm::vec3(radians, radians + shift, radians - shift));
     }
 
     [[nodiscard]] constexpr static Color color(const MyVoxel voxel, const Coord coord) {
         switch (voxel) {
             case MyVoxel::None: return {0.0f, 0.0f, 0.0f, 0.0f};
-            case MyVoxel::Solid: return { glm::vec3(1.0f) - glm::vec3(hash01(coord)), 1.0f};
+            case MyVoxel::Solid: return {rainbow(coord) - glm::vec3(hash01(coord) * 0.1f), 1.0f};
         }
     }
 };
@@ -89,37 +101,33 @@ struct vox::voxel_traits<MyVoxel> {
 
 template<>
 struct vox::voxel_convert_traits<bool, MyVoxel> {
-    [[nodiscard]] constexpr MyVoxel operator()(const bool voxel) {
+    [[nodiscard]] constexpr MyVoxel operator()(const bool voxel) const {
         return voxel ? MyVoxel::Solid : MyVoxel::None;
     }
 };
 
 static vox::VoxelMesh test() {
-    using namespace vox::bool_ops;
-    using enum MyVoxel;
-
-    const auto transform = vox::transform{} <<
-        vox::mirror<vox::MirrorAxis::X>();
-
-    const auto shape =
-        vox::make_sphere({0,0,0},10);
+    using namespace vox;
 
     const auto sampler =
-        vox::convert<MyVoxel>{} <<
-        (transform << shape);
+        transform{}
+        << twist<Axis::Y>(30.0f)
+        << repeat({20, 10, 20})
+        << translate(Coord(-5))
+        << sphere(Coord(), 5.0f);
 
-    const auto mesher = vox::make_blocky_mesher(sampler);
+    const auto mesher = make_blocky_mesher(convert<MyVoxel>{} << sampler);
 
     return mesher({
-        .from = {-64, -16, -64},
-        .to   = {64, 16, 64},
+        .from = {-64, -64, -64},
+        .to   = {64, 64, 64},
     });
 }
 
 int main() {
     using namespace gl;
     if (!glfwInit()) return 1;
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Hello World", nullptr, nullptr);
     if (!window) return 1;
 
     glfwMakeContextCurrent(window);
@@ -144,7 +152,7 @@ int main() {
 
     rend::FirstPersonCamera camera;
     camera.eye    = {0.0f, 0.0f, 20.0f};
-    camera.screen = {640.0f, 480.0f};
+    camera.screen = {1920.0f, 1080.0f};
     camera.yaw = 45.0f;
     camera.far_clip = 500.0f;
 
